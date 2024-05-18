41fd88537498bb4f87c95b373b2408d9dd8bd257
8ptk4 <putte.karlsson@gmail.com>
2024-05-18 14:14:42 +0200

    displaying all of the rooms on the homepage

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,20 +3,73 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { useSession, signIn, signOut } from "next-auth/react";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
+
+function AccountDropdown() {
+	const session = useSession();
+	const isLoggedIn = !!session.data;
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="link">
+					<Avatar className="mr-2">
+						<AvatarImage src={session.data?.user?.image ?? ""} />
+						<AvatarFallback>CN</AvatarFallback>
+					</Avatar>
+
+					{session.data?.user?.name}
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				{isLoggedIn ? (
+					<DropdownMenuItem onClick={() => signOut()}>
+						<LogOutIcon className="mr-2" />
+						Sign Out
+					</DropdownMenuItem>
+				) : (
+					<DropdownMenuItem onClick={() => signIn()}>
+						<LogInIcon className="mr-2" />
+						Sign In
+					</DropdownMenuItem>
+				)}
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
 
 export function Header() {
 	const session = useSession();
 
 	return (
-		<header>
-			<div>
-				{session.data ? (
-					<Button onClick={() => signOut()}>Sign Out</Button>
-				) : (
-					<Button onClick={() => signIn()}>Sign In</Button>
-				)}
-				{session.data?.user?.name}
-				<ModeToggle />
+		<header className="py-2 bg-gray-100 dark:bg-gray-900 container mx-auto">
+			<div className="flex justify-between items-center">
+				<Link
+					href="/"
+					className="flex gap-2 items-center text-xl hover:underline"
+				>
+					<Image
+						src="/programming.png"
+						width="50"
+						height="50"
+						alt="the application icon of a programmer"
+					/>
+					DevFinder
+				</Link>
+				<div className="flex items-center gap-4">
+					<AccountDropdown />
+
+					<ModeToggle />
+				</div>
 			</div>
 		</header>
 	);
